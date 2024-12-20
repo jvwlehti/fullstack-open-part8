8ea6ece87d0f32ee58058fc6eede590adc54ce0f
Jere Lehtinen <93128297+jvwlehti@users.noreply.github.com>
2024-12-20 12:38:31 +0200

    example 8-2 ready

--- examples/part8-2/src/App.jsx
+++ examples/part8-2/src/App.jsx
@@ -1,25 +1,34 @@
-import { gql, useQuery } from '@apollo/client'
-
-const ALL_PERSONS = gql`
-    query {
-        allPersons  {
-            name
-            phone
-            id
-        }
-    }
-`
+import { useQuery } from '@apollo/client'
+import Persons from "../Components/Persons.jsx";
+import PersonForm from "../Components/PersonForm.jsx";
+import React, {useState} from "react";
+import {ALL_PERSONS} from "./queries.jsx";
+import Notify from "../Components/Notify.jsx";
+import PhoneForm from "../Components/PhoneForm.jsx";
 
 const App = () => {
+
+    const [errorMessage, setErrorMessage] = useState(null)
+
     const result = useQuery(ALL_PERSONS)
 
     if (result.loading)  {
         return <div>loading...</div>
     }
 
+    const notify = (message) => {
+        setErrorMessage(message)
+        setTimeout(() => {
+            setErrorMessage(null)
+        }, 10000)
+    }
+
     return (
         <div>
-            {result.data.allPersons.map(p => p.name).join(', ')}
+            <Notify errorMessage={errorMessage} />
+            <Persons persons = {result.data.allPersons} />
+            <PersonForm setError={notify} />
+            <PhoneForm setError={notify} />
         </div>
     )
 }
