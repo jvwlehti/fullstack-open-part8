ffc8756ab65bbe5b346d578d59abe7a16e10ed64
Jere Lehtinen <93128297+jvwlehti@users.noreply.github.com>
2025-01-28 16:16:31 +0200

    separate typedef and resolvers

---
+++ exercises/library-backend/resolvers.js
@@ -0,0 +1,154 @@
+const Book = require("./models/book");
+const Author = require("./models/author");
+const { GraphQLError } = require("graphql/index");
+const mongoose = require("mongoose");
+const User = require("./models/user");
+const jwt = require("jsonwebtoken");
+
+const resolvers = {
+    Query: {
+        bookCount: async () => Book.collection.countDocuments(),
+        authorCount: async () => Author.collection.countDocuments(),
+        allBooks: async (root, args) => {
+            const author = await Author.findOne({ name: args.author })
+
+            const filters = [];
+
+            if (args.author) {
+                filters.push({ author: author.id });
+            }
+
+            if (args.genre) {
+                filters.push({ genres: { $in: [args.genre] } });
+            }
+
+            const query = filters.length > 0 ? { $and: filters } : {};
+            return Book.find(query).populate("author");
+        },
+        allAuthors: async () => Author.find({}),
+        me: (root, args, context) => {
+            return context.currentUser
+        },
+        allGenres: async () =>  {
+            const books = await Book.find({})
+
+            let genres = []
+
+            for (const book of books) {
+                genres.push(...book.genres)
+            }
+
+            genres = [...new Set(genres)]
+
+            return genres
+        }
+    },
+    Author: {
+        bookCount: async (root) => Book.find({author: root.id}).countDocuments(),
+    },
+    Mutation: {
+        addBook: async (root, args, context) => {
+            const currentUser = context.currentUser
+
+            if (!currentUser) {
+                throw new GraphQLError('Not authenticated', {
+                    extensions: {
+                        code: 'BAD_USER_INPUT',
+                    }
+                })
+            }
+
+            try {
+                let author = await Author.findOne({ name: args.author });
+
+                if (!author) author = new Author({ name: args.author });
+                await author.save();
+
+                const book = new Book({ ...args, author });
+                await book.save();
+
+                return book;
+            } catch (err) {
+                let errorMessage = "Saving book failed";
+
+                if (err instanceof mongoose.Error.ValidationError) {
+                    const { name, title } = err.errors;
+
+                    if (name) {
+                        errorMessage = "Saving book failed. Author name is not valid.";
+                    } else if (title) {
+                        errorMessage = "Saving book failed. Book title is not valid.";
+                    }
+
+                    throw new GraphQLError(errorMessage, {
+                        extensions: { code: "BAD_USER_INPUT" },
+                    });
+
+                } else {
+                    throw new GraphQLError(`Unexpected error: ${err.message}`);
+                }
+            }
+        },
+        editAuthor: async (root, args, context) => {
+
+            const currentUser = context.currentUser
+
+            if (!currentUser) {
+                throw new GraphQLError('Not authenticated', {
+                    extensions: {
+                        code: 'BAD_USER_INPUT',
+                    }
+                })
+            }
+
+            const author = await Author.findOne({ name: args.name});
+            if (author) {
+                author.born = args.setBornTo;
+            }
+
+            try {
+                await author.save();
+            } catch (error) {
+                throw new GraphQLError('Update author failed', {
+                    extensions: {
+                        code: "BAD_USER_INPUT",
+                        invalidArgs: args.author,
+                        error
+                    },
+                })
+            }
+            return author
+        },
+        createUser: async (root, args) => {
+            const user = new User({ ...args })
+
+            return user.save()
+                .catch(error => {
+                    throw new GraphQLError('Creating the user failed', {
+                        extensions: {
+                            code: 'BAD_USER_INPUT',
+                            error
+                        }
+                    })
+                })
+        },
+        login: async (root, args) => {
+            const user = await User.findOne({ username: args.username })
+
+            if ( !user || args.password !== 'secret' ) {
+                throw new GraphQLError('wrong credentials', {
+                    extensions: { code: 'BAD_USER_INPUT' }
+                })
+            }
+
+            const userForToken = {
+                username: user.username,
+                id: user._id,
+            }
+
+            return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
+        },
+    }
+}
+
+module.exports = resolvers;
---
+++ exercises/library-backend/schema.js
@@ -0,0 +1,62 @@
+const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: Author!
+    genres: [String!]!
+    id: ID!
+  }
+  
+  type Author {
+    name: String!
+    born : Int
+    id: ID!
+    
+    bookCount: Int!
+  }
+  
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+  
+  type Token {
+    value: String!
+  }
+  
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [Author!]!
+    me: User
+    allGenres: [String!]!
+  }
+  
+  type Mutation {
+    addBook(
+      title: String!,
+      published: Int!,
+      author: String!,
+      genres: [String!]!,
+    ): Book
+    
+    editAuthor(
+      name: String!,
+      setBornTo: Int! 
+    ): Author
+    
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+  
+    login(
+      username: String!
+      password: String!
+    ): Token
+  }
+`
+
+module.exports = typeDefs
